e25280f22d608a2112c7acabbb2b919c8305eedb
Lance Buckley <erchancy@gmail.com>
2023-07-10 11:50:25 -0500

    Added DELETE functionality

--- views/employee_requests.py
+++ views/employee_requests.py
@@ -1,11 +1,13 @@
 EMPLOYEES = [
     {
         "id": 1,
-        "name": "Jenna Solis"
+        "name": "Jenna Solis",
+        "locationId": 1
     },
     {
         "id": 2,
-        "name": "Daniel Myers"
+        "name": "Daniel Myers",
+        "locationId": 2
     }
 ]
 
@@ -46,3 +48,18 @@ def get_single_employee(id):
             requested_employee = employee
 
     return requested_employee
+
+def delete_employee(id):
+    # Initial -1 value for employee index, in case one isn't found
+    employee_index = -1
+
+    # Iterate the employeeS list, but use enumerate() so that you
+    # can access the index value of each item
+    for index, employee in enumerate(EMPLOYEES):
+        if employee["id"] == id:
+            # Found the employee. Store the current index.
+            employee_index = index
+
+    # If the employee was found, use pop(int) to remove it from list
+    if employee_index >= 0:
+        EMPLOYEES.pop(employee_index)
